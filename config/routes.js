9422aa4cea88a34531f34c52cc7ad21121e2a867
Manju KC <Manju KC>
2019-06-14 11:52:37 -0400

    login working

--- config/routes.js
+++ config/routes.js
@@ -29,6 +29,26 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenService.generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!, have a token...`,
+          token,
+  
+        });
+      } else {
+         
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+
 }
 
 function getJokes(req, res) {
