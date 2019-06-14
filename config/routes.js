8d0eaca1431ef9b4be6aa812ab408e6613179285
Manju KC <Manju KC>
2019-06-14 11:19:46 -0400

    implemented user registration

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const bcrypt = require("bcryptjs");
 
+const Users = require("./routes-model.js");
+
 const { authenticate } = require('../auth/authenticate');
 
 module.exports = server => {
@@ -11,6 +13,17 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 5);
+  user.password = hash;
+  Users.add(user)
+    .then(saved => {
+      const token = tokenService.generateToken(user);
+      res.status(201).json({ saved, message: `registered, ${token}` });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
